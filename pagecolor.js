e0728b80b9595cd8c1bce75edb5c954e7f77dfb2
Alok Tamrakar <aloktamrakar2@gmail.com>
2022-10-08 13:29:37 +0530

    intial commit

---
+++ pagecolor.js
@@ -0,0 +1,51 @@
+function red()
+{
+    document.getElementById("p1").style.backgroundColor="red";
+}
+function pink(){
+    document.getElementById("p1").style.backgroundColor="pink";
+}
+function blue()
+{
+    document.getElementById("p1").style.backgroundColor="blue";
+}
+function black()
+{
+    document.getElementById("p1").style.backgroundColor="black";
+}
+function white()
+{
+    document.getElementById("p1").style.backgroundColor="white";
+}
+function yellow()
+{
+    document.getElementById("p1").style.backgroundColor="yellow";
+}
+function purple()
+{
+    document.getElementById("p1").style.backgroundColor="purple";
+}
+function violet()
+{
+    document.getElementById("p1").style.backgroundColor="violet";
+}
+function skyblue()
+{
+    document.getElementById("p1").style.backgroundColor="skyblue";
+}
+function grey()
+{
+    document.getElementById("p1").style.backgroundColor="grey";
+}
+function orange()
+{
+    document.getElementById("p1").style.backgroundColor="orange";
+}
+function brown()
+{
+    document.getElementById("p1").style.backgroundColor="brown";
+}
+function red1()
+{
+    document.getElementById("p1").style.color="red";
+}
---
+++ textcolor.js
@@ -0,0 +1,51 @@
+function red1()
+{
+    document.getElementById("p1").style.color="red";
+}
+function pink1(){
+    document.getElementById("p1").style.color="pink";
+}
+function blue1()
+{
+    document.getElementById("p1").style.color="blue";
+}
+function black1()
+{
+    document.getElementById("p1").style.color="black";
+}
+function white1()
+{
+    document.getElementById("p1").style.color="white";
+}
+function yellow1()
+{
+    document.getElementById("p1").style.color="yellow";
+}
+function purple1()
+{
+    document.getElementById("p1").style.color="purple";
+}
+function violet1()
+{
+    document.getElementById("p1").style.color="violet";
+}
+function skyblue1()
+{
+    document.getElementById("p1").style.color="skyblue";
+}
+function grey1()
+{
+    document.getElementById("p1").style.color="grey";
+}
+function orange1()
+{
+    document.getElementById("p1").style.color="orange";
+}
+function brown1()
+{
+    document.getElementById("p1").style.color="brown";
+}
+function red1()
+{
+    document.getElementById("p1").style.color="red";
+}
